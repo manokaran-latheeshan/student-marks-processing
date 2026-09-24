26053e411f3a41b66119b4f9c3f6dea8c3650ab6
manokaran latheeshan <manokaranlatheeshan1332005@gmail.com>
2026-09-24 23:58:13 +0530

    4th commit

--- Student_Performance.py
+++ Student_Performance.py
@@ -5,9 +5,18 @@ from getData import dataExtract
 
 df=dataExtract()
 
-def Name_Class(ID):
+def perfomance(ID,term):
     Name,Class=df.loc[df["StudentID"]==ID , "Name"].iloc[0] , df.loc[df["StudentID"]==ID , "Class"].iloc[0]
-    return Name,Class
+    subjects=df.loc[(df["StudentID"]==ID) & (df["Term"]==term) , "Subject"].tolist()
+    marks=df.loc[(df["StudentID"]==ID) & (df["Term"]==term) , "Marks"].tolist()
+    average=sum(marks)/len(marks)
+    high_low=[]
+    for sub in subjects:
+        high=df.loc[    (df["Term"]==term)  & (df["Subject"]==sub) , "Marks" ].max()
+        low=df.loc[ (df["Term"]==term)  & (df["Subject"]==sub) , "Marks" ].min()
+        high_low.append(  (high,low)  )
 
-n,c=Name_Class("S001")
-print(n)
+    return subjects,marks,average,high_low,Name,Class
+
+
+
--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 import pandas as pd
 from getData import dataExtract
+from Student_Performance import perfomance
+
+
+while True:
+    print("===========================")
+    print("         M E N U")
+    print("===========================")
+
+    print(f"1. Student Performance\n2. Subject Analysis\n3. Class Analysis\n4. Top 5 Students\n5. At-Risk Students\n6. Subject Difficulty\n7. Search\n8. Exit")
+    
+    print("Choose one of the above (ex: 4)")
+    option=input("Enter here...\n")
+    if option==str(1):
+
+
+
+        
+    elif option == str(8) : 
+        break
+    input("Press any key to return")
+    
 
