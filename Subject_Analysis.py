2a75636887b6b970e88eb7cb5d16e12a8ee108f4
manokaran latheeshan <manokaranlatheeshan1332005@gmail.com>
2026-09-25 03:17:54 +0530

    8th commit

--- Subject_Analysis.py
+++ Subject_Analysis.py
@@ -5,5 +5,19 @@ import pandas as pd
 from getData import dataExtract
 
 
-def sub_analysis(sub):
-    
+def sub_analysis(sub,term):
+    sub=sub.capitalize()
+    df=dataExtract()
+    marks=df.loc[(df["Subject"]==sub) & (df["Term"]==term) , "Marks"]
+    no_std=len(marks)
+    high,low=max(marks),min(marks)
+    avg=sum(marks)/len(marks)
+    Pass,failure=0,0
+    for mark in marks:
+        if mark>=50:
+            Pass+=1
+        else:
+            failure+=1
+    return no_std,avg,high,low,Pass,failure
+
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from getData import dataExtract
 from Student_Performance import perfomance
+from Subject_Analysis import sub_analysis
 
 
 while True:
@@ -25,6 +26,11 @@ while True:
             print(f"{s:<15}{m:>2}{h:12}{l:12}")
         print(f"\nAverage : {round(average,2)}\n")
 
+    elif option==str(2):
+        sub=input("Enter subject")
+        term=input("Enter term")
+        no_std,avg,high,low,Pass,failure=sub_analysis(sub,int(term))
+        print(roun(avg,2))
 
 
     elif option == str(8) : 
