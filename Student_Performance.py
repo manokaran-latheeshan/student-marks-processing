f280dd20300457f2e9443d7748aec8c252ce242b
manokaran latheeshan <manokaranlatheeshan1332005@gmail.com>
2026-09-25 02:13:25 +0530

    5th commit

--- Student_Performance.py
+++ Student_Performance.py
@@ -3,13 +3,17 @@ highest and lowest mark. """
 import pandas as pd
 from getData import dataExtract
 
-df=dataExtract()
 
 def perfomance(ID,term):
+    ID=ID.upper()
+    df=dataExtract()
     Name,Class=df.loc[df["StudentID"]==ID , "Name"].iloc[0] , df.loc[df["StudentID"]==ID , "Class"].iloc[0]
     subjects=df.loc[(df["StudentID"]==ID) & (df["Term"]==term) , "Subject"].tolist()
     marks=df.loc[(df["StudentID"]==ID) & (df["Term"]==term) , "Marks"].tolist()
-    average=sum(marks)/len(marks)
+    if len(marks)!=0:
+        average=sum(marks)/len(marks)
+    else:
+        average=0
     high_low=[]
     for sub in subjects:
         high=df.loc[    (df["Term"]==term)  & (df["Subject"]==sub) , "Marks" ].max()
@@ -18,5 +22,6 @@ def perfomance(ID,term):
 
     return subjects,marks,average,high_low,Name,Class
 
-
+subjects,marks,average,high_low,Name,Class=perfomance("s001",1)
+print(marks)
 
--- main.py
+++ main.py
@@ -12,9 +12,15 @@ while True:
     
     print("Choose one of the above (ex: 4)")
     option=input("Enter here...\n")
+
     if option==str(1):
-
-
+        ID=input("Enter student ID\n").upper()
+        term=int(input("Enter Term\n"))
+        df=dataExtract()
+        subjects,marks,average,high_low,Name,Class=perfomance(ID,term)
+        
+        print(f"\nStudent ID: {ID}\nStudent Name: {Name}")
+        print(f"Subject   || Marks  || Highest || Lowest")
 
         
     elif option == str(8) : 
