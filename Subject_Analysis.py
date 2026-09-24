ef548cfbb75f97ddf53826e17d57fee34e4d472f
manokaran latheeshan <manokaranlatheeshan1332005@gmail.com>
2026-09-25 02:54:24 +0530

    7th commit

---
+++ Subject_Analysis.py
@@ -0,0 +1,9 @@
+'''Given a subject: display number of students, average, highest, lowest, 
+passes and failures '''
+
+import pandas as pd
+from getData import dataExtract
+
+
+def sub_analysis(sub):
+    
