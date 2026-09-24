392c53c756f1be64171dd97fcc434cc66bff2295
manokaran latheeshan <manokaranlatheeshan1332005@gmail.com>
2026-09-24 22:59:00 +0530

    4th commit

--- Student_Performance.py
+++ Student_Performance.py
@@ -1,5 +1,13 @@
-import pandas as pd
-
 """ Given a Student ID: display name, class, subjects, marks, average, 
 highest and lowest mark. """
+import pandas as pd
+from getData import dataExtract
 
+df=dataExtract()
+
+def Name_Class(ID):
+    Name,Class=df.loc[df["StudentID"]==ID , "Name"].iloc[0] , df.loc[df["StudentID"]==ID , "Class"].iloc[0]
+    return Name,Class
+
+n,c=Name_Class("S001")
+print(n)
--- getData.py
+++ getData.py
@@ -1,3 +1,5 @@
 #extract data from csv and make a DataFrame
+import pandas as pd
 def dataExtract():
     df=pd.read_csv("student_data.csv")
+    return df
